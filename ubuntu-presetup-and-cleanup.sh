b92d5138444ec1ef3e21ce65fc0c66f05bc930ce
LucasPedrosoTI <lps1010@gmail.com>
2022-09-29 13:12:31 -0300

    added oh-my-posh configuration

--- ubuntu-presetup-and-cleanup.sh
+++ ubuntu-presetup-and-cleanup.sh
@@ -28,7 +28,20 @@ echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=
 echo "Installing Brave Browser"
 sudo apt update && sudo apt install brave-browser -y
 
+echo "Setting up aliases..."
 echo 'alias cl=clear' >> ~/.bashrc 
+
+echo "Installing and setting up oh my posh"
+sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+sudo chmod +x /usr/local/bin/oh-my-posh
+mkdir ~/.poshthemes
+wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ~/.poshthemes/themes.zip
+unzip ~/.poshthemes/themes.zip -d ~/.poshthemes
+chmod u+rw ~/.poshthemes/*.omp.*
+rm ~/.poshthemes/themes.zip
+oh-my-posh font install FiraCode
+fc-cache -fv
+echo 'eval "$(oh-my-posh init bash --config ~/.poshthemes/1_shell.omp.json)"' >> ~/.bashrc
 
 while true; do
 
